5d77b2400cd7cb6c3c1772ad00c03a23bc008b67
Calvin-Davidson <calvin2003davidson@gmail.com>
2022-11-26 17:18:45 +0100

    Update BlockData.cs

--- Hellia2/Assets/Scripts/Runtime/Grid/BlockData.cs
+++ Hellia2/Assets/Scripts/Runtime/Grid/BlockData.cs
@@ -1,23 +1,11 @@
 using System;
-using SimpleJSON;
 
 namespace Runtime.Grid
 {
-    [System.Serializable]
+    [Serializable]
     public struct BlockData
     {
         public BlockType type;
         public Direction direction;
-
-        public BlockData(JSONNode jsonNode)
-        {
-            type = jsonNode.HasKey("type")
-                ? (BlockType) Enum.Parse(typeof(BlockType), jsonNode["type"].Value)
-                : BlockType.Empty;
-            
-            direction = jsonNode.HasKey("direction")
-                ? (Direction) Enum.Parse(typeof(Direction), jsonNode["direction"].Value)
-                : Direction.None;
-        }
     }
 }
