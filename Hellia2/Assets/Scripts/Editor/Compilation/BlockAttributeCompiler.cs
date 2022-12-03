f5e19bcf7619b47cf4739441d21760d12b258a0e
Calvin-Davidson <calvin2003davidson@gmail.com>
2022-12-03 12:43:48 +0100

    Update BlockAttributeCompiler.cs

--- Hellia2/Assets/Scripts/Editor/Compilation/BlockAttributeCompiler.cs
+++ Hellia2/Assets/Scripts/Editor/Compilation/BlockAttributeCompiler.cs
@@ -1,54 +1,53 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Runtime.Blocks;
 using Runtime.Blocks.Attributes;
 using UnityEditor;
 using UnityEditor.Compilation;
 using UnityEngine;
-using Utilities;
-using Assembly = System.Reflection.Assembly;
 
-public class BlockAttributeCompiler : Editor
+namespace Editor.Compilation
 {
-    /// <summary>
-    /// Subscribe to the assemblyCompilationFinished so we can do custom errors after compiling.
-    /// we use the Finished event as we only want the latest compiled data.
-    /// </summary>
-    [InitializeOnLoadMethod]
-    public static void AddEvent()
+    public class BlockAttributeCompiler : UnityEditor.Editor
     {
-        CompilationPipeline.assemblyCompilationFinished += CheckAttributeSignatures;
-    }
+        /// <summary>
+        /// Subscribe to the assemblyCompilationFinished so we can do custom errors after compiling.
+        /// we use the Finished event as we only want the latest compiled data.
+        /// </summary>
+        [InitializeOnLoadMethod]
+        public static void AddEvent()
+        {
+            CompilationPipeline.assemblyCompilationFinished += CheckAttributeSignatures;
+        }
 
     
-    /// <summary>
-    /// Finds all instances of the BlockAttribute and checks whether the methode signatures are valid.
-    /// if not it will print an error to the console so we are notified of possible bugs within our code base
-    /// </summary>
-    /// <param name="arg1">assembly compilation arugment</param>
-    /// <param name="arg2">compiler messages arror</param>
-    private static void CheckAttributeSignatures(string arg1, CompilerMessage[] arg2)
-    {
-        var extractedMethodes = TypeCache.GetMethodsWithAttribute<BlockAttribute>();
-        
-        foreach (var extractedMethode in extractedMethodes)
+        /// <summary>
+        /// Finds all instances of the BlockAttribute and checks whether the methode signatures are valid.
+        /// if not it will print an error to the console so we are notified of possible bugs within our code base
+        /// </summary>
+        /// <param name="arg1">assembly compilation arugment</param>
+        /// <param name="arg2">compiler messages arror</param>
+        private static void CheckAttributeSignatures(string arg1, CompilerMessage[] arg2)
         {
-            bool isValid = CheckAttributeSignature(extractedMethode);
-            
-            if (!isValid)
+            var extractedMethodes = TypeCache.GetMethodsWithAttribute<BlockAttribute>();
+        
+            foreach (var extractedMethode in extractedMethodes)
             {
-                Debug.LogError(extractedMethode);
+                bool isValid = CheckAttributeSignature(extractedMethode);
+            
+                if (!isValid)
+                {
+                    Debug.LogError(extractedMethode);
+                }
             }
         }
-    }
 
-    private static bool CheckAttributeSignature(MethodInfo methodInfo)
-    {
-        var attribute = methodInfo.CustomAttributes.First(data => data.AttributeType.BaseType == typeof(BlockAttribute));
-        var instance = Activator.CreateInstance(attribute.AttributeType) as BlockAttribute;
-        if (instance == null) return false;
-        return instance.IsValidSignature(methodInfo);
+        private static bool CheckAttributeSignature(MethodInfo methodInfo)
+        {
+            var attribute = methodInfo.CustomAttributes.First(data => data.AttributeType.BaseType == typeof(BlockAttribute));
+            var instance = Activator.CreateInstance(attribute.AttributeType) as BlockAttribute;
+            if (instance == null) return false;
+            return instance.IsValidSignature(methodInfo);
+        }
     }
 }
