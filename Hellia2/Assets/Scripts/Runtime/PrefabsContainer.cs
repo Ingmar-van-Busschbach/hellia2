e31332a0b62cd537b028be38f029ce576616e4d6
Calvin-Davidson <calvin2003davidson@gmail.com>
2022-12-03 16:29:33 +0100

    Update PrefabsContainer.cs

--- Hellia2/Assets/Scripts/Runtime/PrefabsContainer.cs
+++ Hellia2/Assets/Scripts/Runtime/PrefabsContainer.cs
@@ -1,30 +1,30 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Runtime.Blocks;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/BlockPrefabs", order = 1)]
-public class PrefabsContainer : ScriptableObject
+namespace Runtime
 {
-    [SerializeField] private BaseBlock playerPrefab;
-    [SerializeField] private BaseBlock defaultFloorBlock;
-    [SerializeField] private BaseBlock[] moveablePrefabs;
-    [SerializeField] private BaseBlock[] immovablePrefabs;
-    [SerializeField] private BaseBlock[] breakablePrefabs;
-    [SerializeField] private BaseBlock[] meltablePrefabs;
-    [SerializeField] private BaseBlock[] floorPrefabs;
-    [SerializeField] private BaseBlock[] wallPrefabs;
-    [SerializeField] private BaseBlock[] climbablePrefabs;
+    [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/BlockPrefabs", order = 1)]
+    public class PrefabsContainer : ScriptableObject
+    {
+        [SerializeField] private BaseBlock playerPrefab;
+        [SerializeField] private BaseBlock defaultFloorBlock;
+        [SerializeField] private BaseBlock[] moveablePrefabs;
+        [SerializeField] private BaseBlock[] immovablePrefabs;
+        [SerializeField] private BaseBlock[] breakablePrefabs;
+        [SerializeField] private BaseBlock[] meltablePrefabs;
+        [SerializeField] private BaseBlock[] floorPrefabs;
+        [SerializeField] private BaseBlock[] wallPrefabs;
+        [SerializeField] private BaseBlock[] climbablePrefabs;
 
-    public BaseBlock[] ClimbablePrefabs => climbablePrefabs;
-    public BaseBlock[] MoveablePrefabs => moveablePrefabs;
-    public BaseBlock[] ImmovablePrefabs => immovablePrefabs;
-    public BaseBlock[] BreakablePrefabs => breakablePrefabs;
-    public BaseBlock[] MeltablePrefabs => meltablePrefabs;
-    public BaseBlock[] FloorPrefabs => floorPrefabs;
-    public BaseBlock[] WallPrefabs => wallPrefabs;
+        public BaseBlock[] ClimbablePrefabs => climbablePrefabs;
+        public BaseBlock[] MoveablePrefabs => moveablePrefabs;
+        public BaseBlock[] ImmovablePrefabs => immovablePrefabs;
+        public BaseBlock[] BreakablePrefabs => breakablePrefabs;
+        public BaseBlock[] MeltablePrefabs => meltablePrefabs;
+        public BaseBlock[] FloorPrefabs => floorPrefabs;
+        public BaseBlock[] WallPrefabs => wallPrefabs;
 
-    public BaseBlock DefaultFloorBlock => defaultFloorBlock;
-    public BaseBlock PlayerPrefab => playerPrefab;
+        public BaseBlock DefaultFloorBlock => defaultFloorBlock;
+        public BaseBlock PlayerPrefab => playerPrefab;
+    }
 }
