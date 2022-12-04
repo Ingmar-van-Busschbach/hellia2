317df242579052540c21198eb69e6e5ee5327d0f
Calvin-Davidson <calvin2003davidson@gmail.com>
2022-12-04 14:44:06 +0100

    Light system fully working

--- Hellia2/Assets/Scripts/Runtime/Grid/GridManager.cs
+++ Hellia2/Assets/Scripts/Runtime/Grid/GridManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using Runtime.Blocks;
 using UnityEngine;
@@ -22,14 +23,40 @@ namespace Runtime.Grid
 
         public BaseBlock GetFirstBlockInDirection(Vector3Int startPos, Vector3Int direction, int maxDistance)
         {
+            return GetFirstBlockOfTypeInDirection<BaseBlock>(startPos, direction, maxDistance);
+        } 
+        
+        public T GetFirstBlockOfTypeInDirection<T>(Vector3Int startPos, Vector3Int direction, int maxDistance) where T : BaseBlock
+        {
             for (int i = 0; i < maxDistance; i++)
             {
                 BaseBlock block = GetBlockAt(startPos + (direction * i));
                 if (block == null) continue;
-                return block;
+                if (!block.GetType().IsSubclassOf(typeof(T))) continue;
+                return block as T;
             }
 
             return null;
         } 
+
+
+        public List<BaseBlock> GetAllBlocksInDirection(Vector3Int startPos, Vector3Int direction, int maxDistance)
+        {
+            return GetAllBlocksOfTypeInDirection<BaseBlock>(startPos, direction, maxDistance);
+        }
+
+        public List<T> GetAllBlocksOfTypeInDirection<T>(Vector3Int startPos, Vector3Int direction, int maxDistance) where T : BaseBlock
+        {
+            List<T> result = new();
+            for (int i = 0; i < maxDistance; i++)
+            {
+                BaseBlock block = GetBlockAt(startPos + (direction * i));
+                if (block == null) continue;
+                if (block.GetType() != typeof(T)) continue;
+                result.Add(block as T);
+            }
+
+            return result;
+        }
     }
 }
--- Hellia2/Assets/Scripts/Utilities/Directions.cs
+++ Hellia2/Assets/Scripts/Utilities/Directions.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace Utilities
 {
     /// <summary>
@@ -8,12 +10,40 @@ namespace Utilities
     [System.Flags]
     public enum Directions
     {
-        Nothing = 0,
-        Up = 1, // y+
-        Down = 2, // y-
-        Forward = 4, //x+
-        Backward = 8, //x-
-        Left = 16, //z-
-        Right = 32, //z+
+        Nothing = 0, // nothing
+        Up = 1,  // y++
+        Down = 2,  // y--
+        Forward = 4, //z++
+        Backward = 8, //z--
+        Left = 16, //x--
+        Right = 32, //x++
+    }
+
+    public static class DirectionsExtensions
+    {
+        public static Vector3Int ToVector3Int(this Directions directions)
+        {
+            if (directions.HasFlag(Directions.Up)) return Vector3Int.up;
+            if (directions.HasFlag(Directions.Down)) return Vector3Int.down;
+            if (directions.HasFlag(Directions.Left)) return Vector3Int.left;
+            if (directions.HasFlag(Directions.Right)) return Vector3Int.right;
+            if (directions.HasFlag(Directions.Forward)) return Vector3Int.forward;
+            if (directions.HasFlag(Directions.Backward)) return Vector3Int.back;
+            return Vector3Int.zero;
+        }
+        
+        public static Directions Flip(this Directions directions)
+        {
+            if (directions.HasFlag(Directions.Up)) return Directions.Down;
+            if (directions.HasFlag(Directions.Down)) return Directions.Up;
+            
+            if (directions.HasFlag(Directions.Left)) return Directions.Right;
+            if (directions.HasFlag(Directions.Right)) return Directions.Left;
+            
+            if (directions.HasFlag(Directions.Backward)) return Directions.Forward;
+            if (directions.HasFlag(Directions.Forward)) return Directions.Backward;
+
+            return Directions.Nothing;
+        }
     }
 }
